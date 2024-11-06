c0a94e100b5cfe1c90d67d28cd93adb5183aa64b
feng716 <feng716.mssf@gmail.com>
2024-11-06 19:37:33 +0800

    density calculation

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,12 @@ function addVec(vec1 : Coord, vec2 : Coord) {
         y : vec1.y+vec2.y,
     }
 }
+function negVec(vec1 : Coord){
+    return {
+        x : -vec1.x,
+        y : -vec1.y,
+    }
+}
 function norm(vec : Coord){
     return Math.sqrt(vec.x*vec.x+vec.y*vec.y)
 }
@@ -37,43 +43,82 @@ const hashTable : tag2index[] = [];
 const hashFirstIndex : number[] = []
 let timestep = 0;
 type WorldBounds = Coord;
-function getLinearIndex(blockSize : number, wb : WorldBounds, input : Coord) : number{
-    type HashCounts = Coord;
-    const wbBlockCounts : HashCounts = {
-        x: Math.floor(wb.x / blockSize),
-        y: Math.floor(wb.y / blockSize),
-    }
-    const coordBlockCounts : HashCounts = {
+function getHash(blockSize : number, wb : WorldBounds, input : Coord) : Coord{
+    return {
         x: Math.floor(input.x / blockSize),
         y: Math.floor(input.y / blockSize),
     }
-    return coordBlockCounts.y * wbBlockCounts.x + coordBlockCounts.x;
+}
+function getLinearIndex(blockSize : number, wb : WorldBounds, coordBlockCounts : Coord) : number | null{
+    type HashCounts = Coord;
+    if(coordBlockCounts.x >= 0 && coordBlockCounts.x < wb.x/blockSize && coordBlockCounts.y >= 0 && coordBlockCounts.y < wb.y/blockSize){
+        const wbBlockCounts : HashCounts = {
+            x: Math.floor(wb.x / blockSize),
+            y: Math.floor(wb.y / blockSize),
+        }
+        return coordBlockCounts.y * wbBlockCounts.x + coordBlockCounts.x
+    }
+    return null;
+}
+function addHash(diff : Coord, hash : Coord){
+    return {
+        x : hash.x + diff.x,
+        y : hash.y + diff.y,
+    }
 }
 function poly6(r : number, h : number){
     if(r>h) return 0
-    return 315/(64*Math.PI*Math.pow(h,3))*Math.pow((1-(r*r)/(h*h)),3)
+    return 315/(64*Math.PI*Math.pow(h,9))*Math.pow((h*h-r*r),3)
 }
 function d_spiky(vec1 : Coord, h : number){
     let r = norm(vec1);
     return mulVec(vec1, -45/(Math.PI*Math.pow(h,6))*Math.pow((h-r),2)/r)
 }
-function influencePtByNeighbor(particleDst : Pt){
-    
-}
 function addPt2ToPt1Density(particleDst : Pt, particleSrc : Pt){
-    particleDst.density += poly6(norm(addVec(particleSrc.coord, particleDst.coord)), kernelRadius)
+    particleDst.density += poly6(norm(addVec(particleSrc.coord, negVec(particleDst.coord)))/50, kernelRadius)
+}
+function influencePtByNeighbor(particleDst : Pt,fn : (particleDst : Pt, particleSrc : Pt)=>void, blockSize : number, wb : WorldBounds){
+    let dstHash : Coord = getHash(blockSize, wb, particleDst.coord)
+    function forPtInBlock(index : number){
+        for(let i = hashFirstIndex[index - 1]; i < hashFirstIndex[index]; i++){
+            fn(particleDst, particle[hashTable[i].index])
+            drawCircle(particle[hashTable[i].index].coord, 'red')
+        }
+    }
+    function lp(coord : Coord){
+        let hash = getLinearIndex(blockSize, wb, addVec(dstHash, coord));
+        if(hash)
+            forPtInBlock(hash)
+    }
+    lp({x: 0, y: 0})
+    lp({x: -1, y: 0})
+    lp({x: 0, y: -1})
+    lp({x: -1, y: -1})
+    lp({x: 1, y: 0})
+    lp({x: 0, y: 1})
+    lp({x: 1, y: 1})
+    lp({x: -1, y: 1})
+    lp({x: 1, y: -1})
+} 
+function debugBlock(wb :WorldBounds){
+        for(let i = 1; i < 10; i++){
+            ctx?.beginPath()
+            ctx?.moveTo(0, i * 50)
+            ctx?.lineTo(500, i * 50)
+            ctx?.closePath()
+            ctx?.stroke()
+        }
+        for(let i = 1; i < 10; i++){
+            ctx?.beginPath()
+            ctx?.moveTo(i * 50, 0)
+            ctx?.lineTo(i * 50, 500)
+            ctx?.closePath()
+            ctx?.stroke()
+        }
 }
 function solve(dh : number, wb: WorldBounds, blockCounts: number){
     //solve the force
     
-    hashTable.sort((a, b) => a.hash - b.hash)
-    {
-        let k = 0;
-        for(let a = 0; a < blockCounts; a++){
-            while(hashTable[k].hash == a) k++;
-            hashFirstIndex.push(k)
-        }
-    }
     
     particle.map((value: Pt, index:number) => {
         value.force.x = 0
@@ -101,14 +146,19 @@ function solve(dh : number, wb: WorldBounds, blockCounts: number){
         return value
     })
 }
+function drawCircle(coord : Coord, color : CanvasPattern | string | CanvasGradient){
+    ctx?.beginPath()
+    if(ctx){
+        ctx.fillStyle = color
+    }
+    ctx?.arc(coord.x, coord.y, 5, 0, 360)
+    ctx?.closePath()
+    ctx?.fill()
+}
 function draw(particleData:Pt[], wb : WorldBounds){
     ctx?.clearRect(0, 0, wb.x, wb.y)
     for(let a of particleData){
-        ctx?.beginPath()
-        if(ctx) ctx.fillStyle = 'black'
-        ctx?.arc(a.coord.x, a.coord.y, 5, 0, 360)
-        ctx?.closePath()
-        ctx?.fill()
+        drawCircle(a.coord, 'black')
     }
 }
 if(canvas != null && ctx != null){
@@ -120,35 +170,48 @@ if(canvas != null && ctx != null){
             x: parseFloat(canvasWidth),
             y: parseFloat(canvasHeight),
         }
-        for(let i = 0; i < 10; i++){
-            for(let j = 0; j < 10; j++){
-                const randomCoord = {
-                    x : Math.floor(Math.random() * wb.x),
-                    y : Math.floor(Math.random() * wb.y),
-                }
-                particle.push({
-                    coord: randomCoord,
-                    force: {
-                        x: 0,
-                        y: 0,
-                    },
-                    velocity: {
-                        x: 0,
-                        y: 0,
-                    },
-                    density: 0
-                })
+        for(let i = 0; i < 400; i++){
+            const randomCoord = {
+                x : Math.floor(Math.random() * wb.x),
+                y : Math.floor(Math.random() * wb.y),
+            }
+            particle.push({
+                coord: randomCoord,
+                force: {
+                    x: 0,
+                    y: 0,
+                },
+                velocity: {
+                    x: 0,
+                    y: 0,
+                },
+                density: 0
+            })
+            let hash = getLinearIndex(50, wb, getHash(50, wb, randomCoord));
+            if(hash){
                 hashTable.push({
-                    index: i * 10 + j,
-                    hash: getLinearIndex(50, wb, randomCoord),
+                    index: i,
+                    hash: hash,
                 })
             }
         }
-        function process(){
-            //solve(hashTable, 0.01, wb)
-            draw(particle, wb)
-            requestAnimationFrame(process)
+        hashTable.sort((a, b) => a.hash - b.hash)
+        {
+            let k = 0;
+            for(let a = 0; a < 10 * 10; a++){
+                while(k < hashTable.length&&hashTable[k].hash == a) k++;
+                hashFirstIndex.push(k)
+            }
         }
-        requestAnimationFrame(process)
+        
+        particle.map((value) => influencePtByNeighbor(value, addPt2ToPt1Density, 50, wb))
+        draw(particle, wb)
+        debugBlock(wb)
+        //function process(){
+            ////solve(hashTable, 0.01, wb)
+            //draw(particle, wb)
+            //requestAnimationFrame(process)
+        //}
+        //requestAnimationFrame(process)
     }
 }
