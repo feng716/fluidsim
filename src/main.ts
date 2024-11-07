45e392ee94e302cb43969df8765ec4c3bc6b2024
feng716 <feng716.mssf@gmail.com>
2024-11-07 21:03:20 +0800

    pressure force calculation

--- src/main.ts
+++ src/main.ts
@@ -36,11 +36,13 @@ interface tag2index{
     hash : number;
     index : number;
 }
-const particleMass = 1;
+const particleMass = 30;
 const kernelRadius = 1;
+const idealDensity = 1000;
+const stiffness = 8000;
 const particle : Pt[] = [];
-const hashTable : tag2index[] = [];
-const hashFirstIndex : number[] = []
+let hashTable : tag2index[] = [];
+let hashFirstIndex : number[] = []
 let timestep = 0;
 type WorldBounds = Coord;
 function getHash(blockSize : number, wb : WorldBounds, input : Coord) : Coord{
@@ -53,9 +55,9 @@ function getLinearIndex(blockSize : number, wb : WorldBounds, coordBlockCounts :
     type HashCounts = Coord;
     if(coordBlockCounts.x >= 0 && coordBlockCounts.x < wb.x/blockSize && coordBlockCounts.y >= 0 && coordBlockCounts.y < wb.y/blockSize){
         const wbBlockCounts : HashCounts = {
-            x: Math.floor(wb.x / blockSize),
-            y: Math.floor(wb.y / blockSize),
-        }
+                x: Math.floor(wb.x / blockSize),
+                y: Math.floor(wb.y / blockSize),
+            }
         return coordBlockCounts.y * wbBlockCounts.x + coordBlockCounts.x
     }
     return null;
@@ -67,27 +69,33 @@ function addHash(diff : Coord, hash : Coord){
     }
 }
 function poly6(r : number, h : number){
-    if(r>h) return 0
-    return 315/(64*Math.PI*Math.pow(h,9))*Math.pow((h*h-r*r),3)
+    let res = 0.0
+    let r_len = r
+    if (r_len <= h){
+        let tmp = (h * h - r_len * r_len) / (h * h * h)
+        res = 315 / (64 * Math.PI) * tmp * tmp * tmp
+    }
+    return res
 }
-function d_spiky(vec1 : Coord, h : number){
-    let r = norm(vec1);
-    return mulVec(vec1, -45/(Math.PI*Math.pow(h,6))*Math.pow((h-r),2)/r)
-}
-function addPt2ToPt1Density(particleDst : Pt, particleSrc : Pt){
-    particleDst.density += poly6(norm(addVec(particleSrc.coord, negVec(particleDst.coord)))/50, kernelRadius)
+function d_spiky(vec1 : Coord, h : number) : Coord{
+    let res = {x : 0, y : 0}
+    let r_len = norm(vec1)
+    if(r_len<=h){
+        let tmp = (h - r_len) / (h * h * h)
+        res = mulVec(vec1, -45 / Math.PI * tmp * tmp / r_len)
+    }
+    return res
 }
 function influencePtByNeighbor(particleDst : Pt,fn : (particleDst : Pt, particleSrc : Pt)=>void, blockSize : number, wb : WorldBounds){
     let dstHash : Coord = getHash(blockSize, wb, particleDst.coord)
     function forPtInBlock(index : number){
-        for(let i = hashFirstIndex[index - 1]; i < hashFirstIndex[index]; i++){
+        for(let i = index != 0 ? hashFirstIndex[index - 1] : 0; i < hashFirstIndex[index]; i++){
             fn(particleDst, particle[hashTable[i].index])
-            drawCircle(particle[hashTable[i].index].coord, 'red')
         }
     }
     function lp(coord : Coord){
         let hash = getLinearIndex(blockSize, wb, addVec(dstHash, coord));
-        if(hash)
+        if(hash!=null)
             forPtInBlock(hash)
     }
     lp({x: 0, y: 0})
@@ -116,33 +124,72 @@ function debugBlock(wb :WorldBounds){
             ctx?.stroke()
         }
 }
-function solve(dh : number, wb: WorldBounds, blockCounts: number){
+function addPt2ToPt1Pressure(particleDst : Pt, particleSrc : Pt){
+    const pressurePt1 = stiffness * (particleDst.density - idealDensity);
+    const pressurePt2 = stiffness * (particleSrc.density - idealDensity);
+    if(particleDst != particleSrc){
+        particleDst.force = 
+            addVec(
+                particleDst.force,
+                mulVec(
+                    d_spiky( addVec( particleSrc.coord, negVec(particleDst.coord)), kernelRadius),
+                    (pressurePt1/Math.pow(particleDst.density, 2) + pressurePt2 / Math.pow(particleSrc.density, 2)))
+            )
+    }
+}
+function addPt2ToPt1Density(particleDst : Pt, particleSrc : Pt){
+    particleDst.density += particleMass * poly6(norm(addVec(particleSrc.coord, negVec(particleDst.coord)))/5, kernelRadius)
+}
+function solve(dh : number, wb: WorldBounds, blockSize : number){
+    hashFirstIndex = []
+    hashTable.sort((a, b) => a.hash - b.hash)
+    {
+        let k = 0;
+        for(let a = 0; a < wb.x*wb.y/blockSize/blockSize; a++){
+            while(k < hashTable.length&&hashTable[k].hash == a) k++;
+            hashFirstIndex.push(k)
+        }
+    }
+    //calculate the density
+    particle.map((value: Pt) => {
+        value.density = 0
+        influencePtByNeighbor(value, addPt2ToPt1Density, blockSize, wb)
+    })
     //solve the force
-    
     
     particle.map((value: Pt, index:number) => {
         value.force.x = 0
-        value.force.y = 0
-        
-        value.force.y += 98 
-
-        
+        value.force.y = 0 
+        influencePtByNeighbor(value, addPt2ToPt1Pressure, blockSize, wb)
         return value
     })
 
     //integrate the velocity
     particle.map((value: Pt, index:number) => {
-        if(value.coord.x > wb.x || value.coord.x < 0) value.velocity.x *=-0.9;
-        if(value.coord.y > wb.y || value.coord.y < 0) value.velocity.y *=-0.9;
-        value.velocity.x += value.force.x * dh
-        value.velocity.y += value.force.y * dh
+        value.velocity.x = value.force.x * dh
+        value.velocity.y = value.force.y * dh
         return value
     })
     
+    hashTable = []
     //integrate the coord 
     particle.map((value: Pt, index:number) => {
         value.coord.x += value.velocity.x * dh
         value.coord.y += value.velocity.y * dh
+        if(value.coord.x > wb.x || value.coord.x < 0){
+            value.velocity.x *=-0.9;
+            value.coord.x = value.coord.x > 0 ? wb.x : 0
+        } 
+        if(value.coord.y > wb.y || value.coord.y < 0) {
+            value.velocity.y *=-0.9;
+            value.coord.y = value.coord.y > 0 ? wb.y : 0
+        }
+        let hash = getLinearIndex(blockSize, wb, getHash(blockSize, wb, value.coord));
+        if(hash!=null)
+            hashTable.push({
+                index: index,
+                hash: hash,
+            })
         return value
     })
 }
@@ -156,9 +203,9 @@ function drawCircle(coord : Coord, color : CanvasPattern | string | CanvasGradie
     ctx?.fill()
 }
 function draw(particleData:Pt[], wb : WorldBounds){
-    ctx?.clearRect(0, 0, wb.x, wb.y)
+    ctx?.clearRect(0, 0, 500, 500)
     for(let a of particleData){
-        drawCircle(a.coord, 'black')
+        drawCircle(mulVec(a.coord, 50), `rgb(${a.density/1000*255} 0 0)`)
     }
 }
 if(canvas != null && ctx != null){
@@ -167,13 +214,13 @@ if(canvas != null && ctx != null){
     const canvasHeight = canvas.getAttribute("height");
     if(canvasWidth!=null&&canvasHeight!=null){
         const wb : WorldBounds = {
-            x: parseFloat(canvasWidth),
-            y: parseFloat(canvasHeight),
+            x : 10,
+            y : 10
         }
-        for(let i = 0; i < 400; i++){
+        for(let i = 0; i < 1000; i++){
             const randomCoord = {
-                x : Math.floor(Math.random() * wb.x),
-                y : Math.floor(Math.random() * wb.y),
+                x : (Math.random() * wb.x),
+                y : (Math.random() * wb.y),
             }
             particle.push({
                 coord: randomCoord,
@@ -187,31 +234,23 @@ if(canvas != null && ctx != null){
                 },
                 density: 0
             })
-            let hash = getLinearIndex(50, wb, getHash(50, wb, randomCoord));
-            if(hash){
+            let hash = getLinearIndex(1, wb, getHash(1, wb, randomCoord));
+            if(hash!=null){
                 hashTable.push({
                     index: i,
                     hash: hash,
                 })
             }
         }
-        hashTable.sort((a, b) => a.hash - b.hash)
-        {
-            let k = 0;
-            for(let a = 0; a < 10 * 10; a++){
-                while(k < hashTable.length&&hashTable[k].hash == a) k++;
-                hashFirstIndex.push(k)
-            }
-        }
         
-        particle.map((value) => influencePtByNeighbor(value, addPt2ToPt1Density, 50, wb))
+        
         draw(particle, wb)
         debugBlock(wb)
-        //function process(){
-            ////solve(hashTable, 0.01, wb)
-            //draw(particle, wb)
-            //requestAnimationFrame(process)
-        //}
-        //requestAnimationFrame(process)
+        function process(){
+            solve(0.01, wb, 1)
+            draw(particle, wb)
+            requestAnimationFrame(process)
+        }
+        requestAnimationFrame(process)
     }
 }
